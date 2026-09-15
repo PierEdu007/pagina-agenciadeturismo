b638bbbffba40d318f698fae9ff3aa9785168a60
PieroEdu <alccamoron@gmail.com>
2026-09-14 21:13:47 -0500

    chore: agregar ong.html y actualizar vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
+import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   build: {
     rollupOptions: {
-      input: "index.html",
+      input: {
+        main: resolve(import.meta.dirname, "index.html"),
+        ong: resolve(import.meta.dirname, "ong.html"),
+      },
     },
   },
 });
